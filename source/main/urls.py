44e22ec29939d443e7684408df34b3500a5bace1
User <User@Users-MacBook-Pro.local>
2019-09-26 18:56:25 +0600

    Этап 3 сделан.Создал страницы CRUD для типов.

--- source/main/urls.py
+++ source/main/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 from webapp.views import IndexView, TaskCreateView, TaskView, TaskUpdateView, TaskDeleteView, StatusIndexView, \
-    StatusCreateView, StatusUpdateView, StatusDeleteView
+    StatusCreateView, StatusUpdateView, StatusDeleteView, TypeIndexView, TypeCreateView, TypeUpdateView, TypeDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,6 +29,10 @@ urlpatterns = [
     path('status', StatusIndexView.as_view(), name='status'),
     path('status/add/', StatusCreateView.as_view(), name='status_add'),
     path('status/<int:pk>/update', StatusUpdateView.as_view(), name='status_update'),
-    path('status/<int:pk>/delete', StatusDeleteView.as_view(),name='status_delete')
+    path('status/<int:pk>/delete', StatusDeleteView.as_view(), name='status_delete'),
+    path('type', TypeIndexView.as_view(), name='type'),
+    path('type/add/', TypeCreateView.as_view(), name='type_add'),
+    path('type/<int:pk>/update/', TypeUpdateView.as_view(), name='type_update'),
+    path('type/<int:pk>/delete', TypeDeleteView.as_view(), name='type_delete'),
 
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -5,7 +5,7 @@ from django.http import Http404
 
 # from webapp.forms import TaskForm
 from webapp.forms import TaskForm, StatusForm, TypeForm
-from webapp.models import Task, Status
+from webapp.models import Task, Status, Type
 
 
 class IndexView(TemplateView):
@@ -160,3 +160,67 @@ class StatusDeleteView(View):
 
 
 
+class TypeIndexView(TemplateView):
+    template_name = 'type_index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['types'] = Type.objects.all()
+        return context
+
+
+class TypeCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = TypeForm()
+        return render(request, 'type_create.html', context={'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = TypeForm(data=request.POST)
+        if form.is_valid():
+            Type.objects.create(
+                name=form.cleaned_data['name']
+            )
+            return redirect('type')
+        else:
+            return render(request, 'type_create.html', context={'form': form})
+
+
+class TypeUpdateView(View):
+    def get(self, request, *args, **kwargs):
+        type_pk = kwargs.get('pk')
+        type = get_object_or_404(Type, pk=type_pk)
+        form = TypeForm(data={
+            'name': type.name
+        })
+        return render(request, 'type_update.html', context={
+            'form': form,
+            'type': type
+        })
+    def post(self, request, *args, **kwargs):
+        type_pk = kwargs.get('pk')
+        type = get_object_or_404(Type, pk=type_pk)
+        form = TypeForm(data=request.POST)
+        if form.is_valid():
+            type.name = form.cleaned_data['name']
+            type.save()
+            return redirect('type')
+        else:
+            return render(request, 'type_update.html', context={'form': form, 'type': type})
+
+
+
+class TypeDeleteView(View):
+
+    def get(self, request, *args, **kwargs):
+        type_pk = kwargs.get('pk')
+        type = get_object_or_404(Type, pk=type_pk)
+        return render(request, 'type_delete.html', context={'type': type})
+
+    def post(self, request, *args, **kwargs):
+        type_pk = kwargs.get('pk')
+        type = get_object_or_404(Type, pk=type_pk)
+        try:
+            type.delete()
+            return redirect('type')
+        except:
+            raise Exception('Cannot delete type')
