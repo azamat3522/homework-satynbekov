7ae379d41c142f7a1e9cc1868db9c1d4c3d1033f
User <User@Users-MacBook-Pro.local>
2019-10-09 23:43:19 +0600

    Переделал класс Task в моделях, добавил внешний ключ и создал миграцию

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
 
+class Project(models.Model):
+
+    name = models.CharField(max_length=20, verbose_name='Названия проекта')
+
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание проекта')
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+
 class Task(models.Model):
+
+    project = models.ForeignKey('webapp.Project', related_name='tasks', null=True, blank=False,
+                                on_delete=models.PROTECT, verbose_name='Проект')
 
     summary = models.CharField(max_length=200, verbose_name='Заголовок')
 
@@ -36,12 +50,3 @@ class Type(models.Model):
 
 
 
-class Project(models.Model):
-
-    name = models.CharField(max_length=20, verbose_name='Названия проекта')
-
-    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание проекта')
-
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
-
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
