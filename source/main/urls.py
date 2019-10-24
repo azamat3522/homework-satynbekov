a56c5b92ec2bdc853d981871bbfcc076dfe4aaf1
User <User@Users-MacBook-Pro.local>
2019-10-24 16:56:40 +0600

    Разделил urls.py между приложениями.

--- source/main/urls.py
+++ source/main/urls.py
@@ -14,37 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from webapp.views import IndexView, TaskCreateView, TaskView, TaskUpdateView, TaskDeleteView, StatusIndexView, \
-    StatusCreateView, StatusUpdateView, StatusDeleteView, TypeIndexView, TypeCreateView, TypeUpdateView, \
-    TypeDeleteView, ProjectIndexView, ProjectView, ProjectCreateView, ProjectUpdateView, ProjectDeleteView
-
-from accounts.views import login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index'),
-    path('issue/<int:pk>/', TaskView.as_view(), name='task_view'),
-    path('issue/add/', TaskCreateView.as_view(), name="task_add"),
-    path('issue/<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
-    path('issue/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
-    path('status', StatusIndexView.as_view(), name='status'),
-    path('status/add/', StatusCreateView.as_view(), name='status_add'),
-    path('status/<int:pk>/update', StatusUpdateView.as_view(), name='status_update'),
-    path('status/<int:pk>/delete', StatusDeleteView.as_view(), name='status_delete'),
-    path('type', TypeIndexView.as_view(), name='type'),
-    path('type/add/', TypeCreateView.as_view(), name='type_add'),
-    path('type/<int:pk>/update/', TypeUpdateView.as_view(), name='type_update'),
-    path('type/<int:pk>/delete', TypeDeleteView.as_view(), name='type_delete'),
-    path('project', ProjectIndexView.as_view(), name='project'),
-    path('project/<int:pk>/', ProjectView.as_view(), name='project_view'),
-    path('project/add/', ProjectCreateView.as_view(), name='project_create'),
-    path('project/<int:pk>/update', ProjectUpdateView.as_view(), name='project_update'),
-    path('project/<int:pk>/delete', ProjectDeleteView.as_view(), name='project_delete'),
-
-
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout')
+    path('accounts/', include('accounts.urls')),
+    path('', include('webapp.urls'))
 
 ]
