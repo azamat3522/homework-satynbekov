c921ad48ff84d50aff4ece1ae12414d32be4042b
User <User@Users-MacBook-Pro.local>
2019-10-31 17:07:14 +0600

    Добавил личный кабинет

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from accounts.views import login_view, logout_view, register_view
+from accounts.views import login_view, logout_view, register_view, UserDetailView, UserChangeView, \
+    UserChangePasswordView
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
-    path('register/', register_view, name='register')
+    path('register/', register_view, name='register'),
+    path('profile/<pk>/', UserDetailView.as_view(), name='user_detail'),
+    path('profile/<pk>/edit/', UserChangeView.as_view(), name='user_update'),
+    path('profile/<pk>/change-password/', UserChangePasswordView.as_view(), name='user_change_password'),
+
 ]
 
 
