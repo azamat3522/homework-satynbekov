1b6fb8e1952f5a03360c0a4ad0d48ba8ed58e4bc
User <User@Users-MacBook-Pro.local>
2019-11-04 14:09:06 +0600

    Установил Pillow и прописал настройки

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE, verbose_name='Пользователь')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+    about_me = models.TextField(null=True, blank=True, verbose_name='О себе')
+    github_profile = models.URLField(null=True, blank=True, verbose_name='Профиль github')
+
+    def __str__(self):
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
